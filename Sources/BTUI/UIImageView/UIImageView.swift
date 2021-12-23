ae9f18d19a3f5b9c8069369cf6ef8160f0aa9d0a
Fellipe Thufik <fellipe_thufik@hotmail.com>
2021-12-23 13:36:32 -0300

    test textfield

---
+++ Sources/BTUI/UIImageView/UIImageView.swift
@@ -0,0 +1,26 @@
+//
+//  UIImageView.swift
+//  
+//
+//  Created by Fellipe thufik Costa Gomes Hoashi on 23/12/21.
+//
+
+import UIKit
+
+public class headerImageView: UIImageView {
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+       image = UIImage(named: "headerImage")
+    }
+}
+
+
+
